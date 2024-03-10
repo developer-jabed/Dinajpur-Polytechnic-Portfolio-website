d148379e16d6886548f8298cfb3cf703dae0be17
developer-jabed <jabed1780@gmail.com>
2024-03-10 22:03:56 +0600

    added short animation

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    String:["You can See ","Student Qualification", "And Many More"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
